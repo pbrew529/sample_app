26fc308a048d9797795c3529d4212c69b5eb6837
pbrew529 <piersbrew@gmail.com>
2012-12-26 14:52:03 -0500

    Tests Controllers and View

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,11 +7,35 @@ describe "StaticPages" do
 	
 	it "should have the content 'Sample App'" do
 		
-		visit 'static_pages/home'
+		visit '/static_pages/home'
 		page.should have_content('Sample App')
 		
 	end
   
   end
  
-end
+  describe "Help Page" do
+ 
+ 
+	it "should have the content 'Help'" do
+	
+		visit '/static_pages/help'
+		page.should have_content('Help')
+	
+	end
+
+ 
+ end
+
+  describe "About Page" do
+  
+	it "Should have the content 'About Us'" do
+	
+		visit '/static_pages/about'
+		page.should have_content('About Us')
+	
+	end
+  
+  end
+ 
+ end
